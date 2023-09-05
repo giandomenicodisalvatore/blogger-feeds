29abcab2d328c53764c23c1b5c58074b48d82e6a
Giandomenico Di Salvatore <giandomenicodisalvatore@gmail.com>
2023-09-06 00:55:01 +0200

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			'@lib': resolve(__dirname, './src/lib/index'),
+		},
+	},
+
+	plugins: [
+		dts({
+			entryRoot: './src',
+			insertTypesEntry: true,
+		}),
+	],
+
+	build: {
+		outDir: './dist',
+		emptyOutDir: true,
+
+		lib: {
+			entry: resolve('./src/main.ts'),
+			formats: ['es'],
+			name: 'blogspot-feeds',
+		},
+	},
+
+	test: {
+		globals: true,
+		outputFile: './www/tests/index.html',
+
+		dir: './tests',
+
+		reporters: ['default', 'basic', 'json', 'html'],
+		cache: {
+			dir: '../node_modules/.vitest',
+		},
+
+		coverage: {
+			reporter: ['html', 'json', 'text'],
+			reportsDirectory: './www/coverage',
+			enabled: true, // only prod
+			provider: 'v8',
+		},
+	},
+})
